c38d9cab6cef24d570004c8f1052f2ea0e3e3a3d
dameraforce <40764222+dameraforce@users.noreply.github.com>
2018-08-23 17:04:00 +0530

    Update server.js

--- src/app/server.js
+++ src/app/server.js
@@ -1,27 +1,15 @@
-const path = require('path');
+//Install express server
 const express = require('express');
+const path = require('path');
 const app = express();
 
-let env = process.env.NODE_ENV || 'development';
-
-let forceSSL = (req, res, next) => {
-  if (req.headers['x-forwarded-proto'] !== 'https') {
-    return res.redirect(['https://', req.get('Host'), req.url].join(''));
-  }
-  return next();
-};
-
-if (env === 'production') {
-  app.use(forceSSL);
-}
-
-// Serve static files
+// Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/scheduler'));
 
-// Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/scheduler/index.html'));
+app.get('/*', function(req,res) {
+
+  res.sendFile(path.join(__dirname+'/dist/scheduler/index.html'));
 });
 
-// default Heroku port
-app.listen(process.env.PORT || 5000);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
